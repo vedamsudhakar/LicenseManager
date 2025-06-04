a11be92d1270da4f12c9916f81574a366dceab57
Sudhakar Vedam <Sudhakar.Vedam@kofax.com>
2025-06-04 18:50:29 +0530

    Applications controller, Features controller added.

---
+++ LicenseManager/Controllers/ApplicationsController.cs
@@ -0,0 +1,152 @@
+﻿using LicenseManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LicenseManager.Controllers
+{
+    public class ApplicationsController : Controller
+    {
+        private readonly LicenseManagerContext _context;
+
+        public ApplicationsController(LicenseManagerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Applications
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Applications.ToListAsync());
+        }
+
+        // GET: Applications/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var application = await _context.Applications
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (application == null)
+            {
+                return NotFound();
+            }
+
+            return View(application);
+        }
+
+        // GET: Applications/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Applications/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] Application application)
+        {
+            if (ModelState.IsValid)
+            {
+                application.Id = Guid.NewGuid();
+                _context.Add(application);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(application);
+        }
+
+        // GET: Applications/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var application = await _context.Applications.FindAsync(id);
+            if (application == null)
+            {
+                return NotFound();
+            }
+            return View(application);
+        }
+
+        // POST: Applications/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Name")] Application application)
+        {
+            if (id != application.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(application);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ApplicationExists(application.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(application);
+        }
+
+        // GET: Applications/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var application = await _context.Applications
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (application == null)
+            {
+                return NotFound();
+            }
+
+            return View(application);
+        }
+
+        // POST: Applications/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var application = await _context.Applications.FindAsync(id);
+            if (application != null)
+            {
+                _context.Applications.Remove(application);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ApplicationExists(Guid id)
+        {
+            return _context.Applications.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ LicenseManager/Controllers/FeaturesController.cs
@@ -0,0 +1,160 @@
+﻿using LicenseManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace LicenseManager.Controllers
+{
+    public class FeaturesController : Controller
+    {
+        private readonly LicenseManagerContext _context;
+
+        public FeaturesController(LicenseManagerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Features
+        public async Task<IActionResult> Index()
+        {
+            var licenseManagerContext = _context.Features.Include(f => f.FkFeatureGroup);
+            return View(await licenseManagerContext.ToListAsync());
+        }
+
+        // GET: Features/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var feature = await _context.Features
+                .Include(f => f.FkFeatureGroup)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (feature == null)
+            {
+                return NotFound();
+            }
+
+            return View(feature);
+        }
+
+        // GET: Features/Create
+        public IActionResult Create()
+        {
+            ViewData["FkFeatureGroupId"] = new SelectList(_context.FeatureGroups, "Id", "Id");
+            return View();
+        }
+
+        // POST: Features/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FkFeatureGroupId,Name")] Feature feature)
+        {
+            if (ModelState.IsValid)
+            {
+                feature.Id = Guid.NewGuid();
+                _context.Add(feature);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FkFeatureGroupId"] = new SelectList(_context.FeatureGroups, "Id", "Id", feature.FkFeatureGroupId);
+            return View(feature);
+        }
+
+        // GET: Features/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var feature = await _context.Features.FindAsync(id);
+            if (feature == null)
+            {
+                return NotFound();
+            }
+            ViewData["FkFeatureGroupId"] = new SelectList(_context.FeatureGroups, "Id", "Id", feature.FkFeatureGroupId);
+            return View(feature);
+        }
+
+        // POST: Features/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,FkFeatureGroupId,Name")] Feature feature)
+        {
+            if (id != feature.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(feature);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FeatureExists(feature.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FkFeatureGroupId"] = new SelectList(_context.FeatureGroups, "Id", "Id", feature.FkFeatureGroupId);
+            return View(feature);
+        }
+
+        // GET: Features/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var feature = await _context.Features
+                .Include(f => f.FkFeatureGroup)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (feature == null)
+            {
+                return NotFound();
+            }
+
+            return View(feature);
+        }
+
+        // POST: Features/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var feature = await _context.Features.FindAsync(id);
+            if (feature != null)
+            {
+                _context.Features.Remove(feature);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FeatureExists(Guid id)
+        {
+            return _context.Features.Any(e => e.Id == id);
+        }
+    }
+}
