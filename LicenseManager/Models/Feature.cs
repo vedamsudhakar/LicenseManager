28bc09c30e62352afb43c8cd43f434a5bc6c19db
Bantupalli Venkateswararao <venkateswararao.bantupalli@kofax.com>
2025-06-05 11:33:17 +0530

    Features - Changed the display values from Id's to Name

--- LicenseManager/Models/Feature.cs
+++ LicenseManager/Models/Feature.cs
@@ -1,12 +1,19 @@
-﻿namespace LicenseManager.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LicenseManager.Models;
 
 public partial class Feature
 {
+    [Display(Name = "Id")]
+
     public Guid Id { get; set; }
 
+    [Display(Name = "Feature Group Id")]
     public Guid FkFeatureGroupId { get; set; }
 
+    [Display(Name = "Feature Group")]
     public string Name { get; set; } = null!;
 
-    public virtual FeatureGroup FkFeatureGroup { get; set; } = null!;
+    [Display(Name ="Feature Group")]
+    public virtual FeatureGroup? FkFeatureGroup { get; set; } = null!;
 }
